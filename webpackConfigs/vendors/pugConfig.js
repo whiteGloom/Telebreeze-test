2ba356ee46ff783a59ce62d3ee4e626e69526b8b
whiteGloom <karabin.m@yandex.ru>
2019-12-12 20:55:59 +0700

    Data update
    Список изменений
    • задел под получения данных со стороны;
    • правка подключения стилей;
    • правка стилей меню;

--- webpackConfigs/vendors/pugConfig.js
+++ webpackConfigs/vendors/pugConfig.js
@@ -1,6 +1,45 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
 
+function getData() {
+  return {
+    mainData: {
+      majorTitle: 'Welcome to our site!',
+      minorTitle: 'We are always glad to see you!',
+      imageSrc: './static/images/some3.jpg',
+    },
+    menuData: {
+      title: "Blocks",
+      groups: [
+        {
+          title: 'Til Satan ære',
+          items: [
+            {image: {alt: 'someImage', src: './static/images/some.jpeg'}, href: '#'},
+            {image: {alt: 'someImage', src: './static/images/some.jpeg'}, href: '#'},
+            {image: {alt: 'someImage', src: './static/images/some.jpeg'}, href: '#'},
+          ],
+        },
+        {
+          title: 'Zur Ehre des Satans',
+          items: [
+            {image: {alt: 'someImage', src: './static/images/some1.jpg'}, size: 'box', href: '#'},
+            {image: {alt: 'someImage', src: './static/images/some1.jpg'}, size: 'box', href: '#'},
+            {image: {alt: 'someImage', src: './static/images/some1.jpg'}, size: 'box', href: '#'},
+          ],
+        },
+        {
+          title: 'Satanas in gloria est scriptor',
+          items: [
+            {image: {alt: 'someImage', src: './static/images/some2.jpg'}, href: '#'},
+            {image: {alt: 'someImage', src: './static/images/some2.jpg'}, href: '#'},
+            {image: {alt: 'someImage', src: './static/images/some2.jpg'}, href: '#'},
+          ],
+        },
+      ],
+    },
+  }
+}
+
 function config(options = {}) {
   const { workFolder } = options;
 
@@ -10,6 +49,9 @@ function config(options = {}) {
         {
           test: /\.(pug|html)/,
           loader: 'pug-loader',
+          options: {
+            self: true,
+          },
           exclude: /[\\/]node_modules[\\/]/,
         },
       ],
@@ -20,6 +62,7 @@ function config(options = {}) {
         filename: 'index.html',
         inject: false,
         minify: false,
+        data: getData(),
       }),
       new HtmlBeautifyPlugin({
         config: {
