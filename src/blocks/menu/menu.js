7ceadde3792535e1c5b19db90a27ecc361b67178
whiteGloom <karabin.m@yandex.ru>
2019-12-12 18:37:56 +0700

    Main block update
    Список изменений:
    • написан основной блок сайта;
    • правки стилей;
    • заготовка для хедера;
    • обновление меню;

--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,6 +1,17 @@
-(function() {
+function init() {
+  const menu = document.getElementById('menu');
+  const closeButton = menu.getElementsByClassName('menu__close-button')[0];
+  function hideMenu() {
+    menu.classList.remove('menu_state_open');
+    menu.classList.add('menu_state_hidden');
+  }
+
+  closeButton.addEventListener('click', hideMenu);
+
   setTimeout(() => {
-    document.getElementsByClassName('menu')[0].classList.remove('menu_state_hidden');
-    document.getElementsByClassName('menu')[0].classList.add('menu_state_open');
+    menu.classList.remove('menu_state_hidden');
+    menu.classList.add('menu_state_open');
   }, 1500);
-}());
+};
+
+document.addEventListener('DOMContentLoaded', init);
--- src/statics.js
+++ src/statics.js
@@ -3,6 +3,7 @@ function importAll(r) {
   r.keys().forEach((key) => { cache[key] = r(key); });
 }
 
-importAll(require.context('./static/', true, /\.(jpe?g|gif|png|svg|ico)/)); // images
+importAll(require.context('./blocks/', true, /\.(jpe?g|gif|png|svg|ico)/)); // images
+importAll(require.context('./static/images/', true, /\.(jpe?g|gif|png|svg|ico)/)); // images
 importAll(require.context('./static/styles/', true, /\.(less|css)/)); // styles
 importAll(require.context('./blocks/', true, /\.(less|css)/)); // styles
